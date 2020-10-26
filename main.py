62718a1c73342e26c6cf96e883a947f76a4613d9
loot-06 <73281546+loot-06@users.noreply.github.com>
2020-10-26 22:14:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import discord
 from discord.ext import commands
+from VB_project.messages import give_msg
+TOKEN = "SECRET TOKEN "
 
-TOKEN = "SETCER TOKEN"
+app = commands.Bot(command_prefix="v!")
 
-app = commands.Bot(command_prefix="`")
 
-@app.event # start code
-async def startup():
-  game = discord.Game("`도움")
-  await app.change_presence(status=discord.Status.online, activity=game)
- 
- 
+@app.event  # start code
+async def on_ready():
+    print("startup!")
+    game = discord.Game("v!명령어 , v!도움 | For Gamers") # v!명령어 , v!도움 | For Gamers
+    await app.change_presence(status=discord.Status.online, activity=game)
+
+
+app.remove_command("help")
+@app.command(name="명령어", alisases=("help","도움"))
+async def help(ctx, contents="help"):
+
+
+    embed = discord.Embed(
+        title="도움말",
+        description=give_msg(contents),
+        colour=0x4262F4
+    )
+
+    await ctx.send(embed=embed)
+
+if __name__ == "__main__":
+    app.run(TOKEN)
